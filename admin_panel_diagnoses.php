a4de96b3162ae11edd1e5a34fa65ed8e7cfcf76f
David Sellam <dvdsellam@gmail.com>
2019-07-14 13:20:51 +0300

    f

--- admin_panel_diagnoses.php
+++ admin_panel_diagnoses.php
@@ -105,14 +105,14 @@
     <main>
     <section id="users">
     <div class="back_to_admin_panel">
-  <button class="pure-material-button-contained"onclick="window.location.href='admin_panel.php'"> Back to admin panel </button> 
+  <button class="pure-material-button-contained"onclick="window.location.href='admin_panel.php'"> Back to admin panel </button>
 </div>
     <h1>Diagnoses Management</h1>
 
     <div class="container">
   <div class="row justify-content-md-center">
     <div class="col_diagnose">
-<table class="table table-hover table-striped table-responsive user-panel"> 
+<table class="table table-hover table-striped table-responsive user-panel">
    <thead>
     <tr>
       <th>ID</th>
@@ -126,7 +126,7 @@
   <?php
          $query = "SELECT * FROM tbl_diseases_221";
          $result = mysqli_query($connection , $query);
-         while($row = mysqli_fetch_assoc($result)) 
+         while($row = mysqli_fetch_assoc($result))
          {
          echo'
     <tr>
@@ -140,7 +140,7 @@
       echo'
    </tr>
   </tbody>
-</table> 
+</table>
 </div>
 </div>
 </div>';
@@ -194,7 +194,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="includes/js/main.js"></script>
   </body>
 </html>
 <?php
